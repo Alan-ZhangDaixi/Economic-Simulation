97de4141a8405093ab826372f0df3c7e802ee2c9
Zhang Daixi <335417490@qq.com>
2018-05-23 17:37:30 +0800

    add some model cs

--- Assets/Economic System/Scripts/Model/EconomicSystemModel.cs
+++ Assets/Economic System/Scripts/Model/EconomicSystemModel.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using UnityEngine;
 
-namespace Assets.Economic_System.Scripts.Model
+public class EconomicSystemModel : MonoBehaviour
 {
-    class EconomicSystemModel
-    {
-    }
+
 }
+
---
+++ Assets/Economic System/Scripts/Model/Player/EcoPlayerModel.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class EcoPlayerModel : MonoBehaviour
+{
+    public EcoPlayerData data { get; private set; }
+
+    #region mono functions
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        UpdatePos();
+    }
+
+    #endregion mono functions
+
+    #region base function
+
+    public void Init(EcoPlayerData data)
+    {
+        this.data = data;
+    }
+
+    #endregion base function
+
+    #region functions
+
+    void UpdatePos()
+    {
+        transform.position = data.localPos;
+    }
+
+    #endregion functions
+}
